7c5d1e9dd8fe84fb773de5b5fb2546af72fda10d
Avenart <info@avenart.com>
2014-06-06 23:40:18 +0100

    Removing some more CI files

--- application/views/footer.php
+++
@@ -1,9 +0,0 @@
-      <footer>
-        <hr />
-        <p>2014 &copy; <?=site_name()?> by <a href="http://www.avenart.com/" target="_blank">Avenart Design</a></p>
-      </footer>
-
-    </div> <!-- /container -->
-
-  </body>
-</html>
--- application/views/home.php
+++
@@ -1,6 +0,0 @@
-      <!-- Jumbotron -->
-      <div class="jumbotron">
-        <h1>CI dev chat</h1>
-        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        <a class="btn btn-large btn-success" href="<?php echo base_url('auth/login'); ?>">Get started today</a>
-      </div>
